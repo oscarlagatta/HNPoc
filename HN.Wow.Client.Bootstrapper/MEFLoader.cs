9494fc65692c2cf7caf58330cc3ab5cac1c86032
Oscar Lagatta <oscarlagatta@gmail.com>
2015-06-08 23:16:19 +0100

    Client Bootstrapper and Client Contracts (previous commit didn't take them)

---
+++ HN.Wow.Client.Bootstrapper/MEFLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using HN.Wow.Client.Proxies;
+
+namespace HN.Wow.Client.Bootstrapper
+{
+    public static class MEFLoader
+    {
+        public static CompositionContainer Init()
+        {
+            return Init(null);
+        }
+
+        public static CompositionContainer Init(ICollection<ComposablePartCatalog> catalogParts)
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(ProductClient).Assembly));
+
+
+            if (catalogParts != null)
+                foreach (var part in catalogParts)
+                    catalog.Catalogs.Add(part);
+
+            CompositionContainer container = new CompositionContainer(catalog);
+
+            return container;
+        }
+
+    }
+}
---
+++ HN.Wow.Client.Contracts/Service Contracts/IProductService.cs
@@ -0,0 +1,49 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using HN.Wow.Client.Entities;
+
+namespace HN.Wow.Client.Contracts
+{
+    [ServiceContract]
+    public interface IProductService : IServiceContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Product GetProduct(int productId);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Product[] GetAllProducts();
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Product UpdateProdcut(Product product);
+
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void DeleteProduct(int ProductId);
+
+
+        #region Async Operations
+
+        [OperationContract]
+        Task<Product> GetProductAsync(int productId);
+
+        [OperationContract]
+        Task<Product[]> GetAllProductsAsync();
+
+        [OperationContract]
+        Task<Product> UpdateProductAsync(Product product);
+
+        [OperationContract]
+        Task DeleteProductAsync(int productId);
+
+        #endregion
+    }
+}
