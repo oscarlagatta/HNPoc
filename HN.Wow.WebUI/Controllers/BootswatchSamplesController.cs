47d80585c9c4234d42821e2060a9f5627ca8e176
Oscar Lagatta <oscarlagatta@gmail.com>
2015-07-06 13:53:59 +0100

    Bootswatch Samples

---
+++ HN.Wow.WebUI/Controllers/BootswatchSamplesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Bootstrap_Libraries.Controllers
+{
+    public class BootswatchSamplesController : Controller
+    {
+        public ActionResult DarklySample()
+        {
+            return View();
+        }
+
+        public ActionResult FlatlySample()
+        {
+            return View();
+        }
+
+        public ActionResult SandstoneSample()
+        {
+            return View();
+        }
+
+        public ActionResult LumenSample()
+        {
+            return View();
+        }
+    }
+}
