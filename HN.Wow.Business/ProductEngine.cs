b47ca9002c29de078d11db5ea5bb2346d9b59ffd
Oscar Lagatta <oscarlagatta@gmail.com>
2015-07-12 12:48:33 +0100

    Added Business project and Hosting Console application

---
+++ HN.Wow.Business/ProductEngine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Contracts;
+using HN.Wow.Business.Common;
+using HN.Wow.Business.Entities;
+using HN.Wow.Data.Contracts;
+
+namespace HN.Wow.Business
+{
+    [Export(typeof(IProductEngine))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class ProductEngine
+    {
+        [ImportingConstructor]
+        public ProductEngine(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _dataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        IDataRepositoryFactory _dataRepositoryFactory;
+
+        public bool isProductAvailable(int productId)
+        {
+            bool available = false;
+
+            IProductRepository productRepository = _dataRepositoryFactory.GetDataRepository<IProductRepository>();
+
+            var currentProdcut = productRepository.Get(productId);
+
+            if (currentProdcut != null && currentProdcut.Stock > 0)
+                available = true;
+
+            return available;
+        }
+
+        public bool isProductAvailable(int productId, DateTime createdOn, DateTime currentDateTime,
+            IEnumerable<Product> products)
+        {
+            bool available = true;
+            /*
+             * We write all the business logic necessary to obtain the value for "available"
+             * 
+             * 1.- Check the Stock 
+             
+             * 2.- Check additional logic
+             */
+
+            Product product = products.Where(item => item.ProductId == productId).FirstOrDefault();
+            if (product != null && product.Stock == 0)
+                available = false;
+
+            /*
+             * Example 2
+             * The method could receive also a list of Reserved products
+             * 
+             * Reservation reservation = reservedProducts.Where( item => item.ProductId == productId).FirstOrDefault();
+             * if( reservation != null && ( 
+             *      ( createdOn >= reservation.ReservedDate && pickupDate <= reservation.ReturnDate))
+             *      available = false;
+             */
+
+            return available;
+        }
+
+        public bool isProductCurrentlyReserved(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isProductCurrentlyDispatched(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HN.Wow.ServiceHost.Console/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Core;
+using HN.Wow.Business.Bootstrapper;
+using HN.Wow.Business.Managers.Managers;
+using SM = System.ServiceModel;
+using System.Transactions;
+namespace HN.Wow.ServiceHost.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectBase.Container = MEFLoader.Init();
+
+            System.Console.WriteLine("Starting up services");
+            System.Console.WriteLine("");
+
+            SM.ServiceHost hostProductManger = new SM.ServiceHost(typeof(ProductManager));
+            
+            /* More services to call  */
+            StartService(hostProductManger, "ProductManager Host");
+       
+            System.Timers.Timer timer = new System.Timers.Timer(10000);
+            timer.Elapsed += OnTimerElapsed;
+            timer.Start();
+
+            System.Console.WriteLine("eCommerce Monitor has started.");
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("Press [Enter] to exit.");
+            System.Console.ReadLine();
+
+            timer.Stop();
+            System.Console.WriteLine("eCommerce Monitor stopped.");
+
+            StopService(hostProductManger, "AccountManager Host");
+          
+        }
+
+        private static void OnTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            System.Console.WriteLine("Looking for Products at {0} ", DateTime.Now.ToString());
+
+            ProductManager productManager = new ProductManager();
+
+            var products = productManager.GetAllProducts();
+
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    using (TransactionScope scope = new TransactionScope())
+                    {
+                        try
+                        {
+                            //productManager.DeleteProduct(product.ProductId);
+                            System.Console.WriteLine("Deleting Product '{0}'.", product.ProductName);
+                            scope.Complete();
+                        }
+                        catch (Exception)
+                        {
+                            System.Console.WriteLine("There was an excpetion when attempting to delete product {0}", product.ProductId);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private static void StartService(SM.ServiceHost host, string serviceDescription)
+        {
+            host.Open();
+
+            System.Console.WriteLine("Service {0} started.", serviceDescription);
+
+
+            foreach (var endpoint in host.Description.Endpoints)
+            {
+                System.Console.WriteLine(string.Format("Listening on endpoint:"));
+                System.Console.WriteLine(string.Format("Address: {0}", endpoint.Address.Uri.ToString()));
+                System.Console.WriteLine(string.Format("Binding: {0}", endpoint.Binding.Name));
+                System.Console.WriteLine(string.Format("Contract: {0}", endpoint.Contract.ConfigurationName));
+            }
+
+            System.Console.WriteLine();
+        }
+
+        private static void StopService(SM.ServiceHost host, string serviceDescription)
+        {
+            host.Close();
+            System.Console.WriteLine("Service {0} stopped.", serviceDescription);
+        }
+    }
+}
